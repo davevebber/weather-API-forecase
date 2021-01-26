4f0092fdc5f805423a469ab8164dad79583615da
dave vebber <davevebber@gmail.com>
2021-01-26 14:07:53 -0800

    moment.js added and now displays dates on weather info

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,22 @@
 //const 
 const apiKey = 'd748733e2081840aba6654717752a32b';
 const searchBtn = document.querySelector('#searchBtn');
+const currentBox = document.querySelector('.current-box');
+const forecastBox = document.querySelector('.forecast-box')
+
+// dates
+let date = (moment().format('ddd, MMM Do'));
+let dateOne = moment().add(1,'days').format('MMM Do');
+let dateTwo = moment().add(2,'days').format('MMM Do');
+let dateThree = moment().add(3,'days').format('MMM Do');
+let dateFour = moment().add(4,'days').format('MMM Do');
 
 // variables
 let searchArr = []
 
 // for current day weather
 let cityName = document.querySelector('#cityName');
+let currentDate = document.querySelector('#date');
 let temp = document.querySelector('#temp');
 let humid = document.querySelector('#humid');
 let wind = document.querySelector('#wind');
@@ -35,6 +45,9 @@ let dayFourUV = document.querySelector('#dayFourUV');
 
 
 function getCityWeather() {
+    currentBox.classList.remove('hide');
+    forecastBox.classList.remove('hide');
+
     let searchCity = document.querySelector('#searchCity').value;
     localStorage.setItem('searchHistory', searchCity);
 
@@ -45,10 +58,10 @@ function getCityWeather() {
         .then(function (data) {
 
             // current day weather info
-            cityName.innerHTML = data.name;
+            cityName.innerHTML = data.name + ': ' + date;
             temp.innerHTML = 'Temperature: ' + data.main.temp + '°F';
             humid.innerHTML = 'Humidity: ' + data.main.humidity + '%';
-            wind.innerHTML = 'Wind Speed: ' + data.wind.speed + 'MPH';
+            wind.innerHTML = 'Wind Speed: ' + data.wind.speed + ' MPH';
 
             fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&units=imperial' + '&APPID=' + apiKey)
                 .then(function (response) {
@@ -58,21 +71,29 @@ function getCityWeather() {
                     console.log(data);
                     // current day UV index
                     uv.innerHTML = 'UV Index: ' + data.current.uvi
+
                     // forecast day 1
-                    dayOneTemp.innerHTML = 'Temp: ' + data.daily[0].temp.day;
-                    dayOneWind.innerHTML = 'Wind Speed: ' + data.daily[0].wind_speed;
+                    dayOneDate.innerHTML = dateOne;
+                    dayOneTemp.innerHTML = 'Temp: ' + data.daily[0].temp.day + '°F';
+                    dayOneWind.innerHTML = 'Wind Speed: ' + data.daily[0].wind_speed + ' MPH';
                     dayOneUV.innerHTML = 'UV Index: ' + data.daily[0].uvi;
+                   
                     // forecast day 2
-                    dayTwoTemp.innerHTML = 'Temp: ' + data.daily[1].temp.day;
-                    dayTwoWind.innerHTML = 'Wind Speed: ' + data.daily[1].wind_speed;
+                    dayTwoDate.innerHTML = dateTwo;
+                    dayTwoTemp.innerHTML = 'Temp: ' + data.daily[1].temp.day + '°F';
+                    dayTwoWind.innerHTML = 'Wind Speed: ' + data.daily[1].wind_speed + ' MPH';
                     dayTwoUV.innerHTML = 'UV Index: ' + data.daily[1].uvi;
+                   
                     // forecast day 3
-                    dayThreeTemp.innerHTML = 'Temp: ' + data.daily[2].temp.day;
-                    dayThreeWind.innerHTML = 'Wind Speed: ' + data.daily[2].wind_speed;
+                    dayThreeDate.innerHTML = dateThree;
+                    dayThreeTemp.innerHTML = 'Temp: ' + data.daily[2].temp.day + '°F';
+                    dayThreeWind.innerHTML = 'Wind Speed: ' + data.daily[2].wind_speed + ' MPH';
                     dayThreeUV.innerHTML = 'UV Index: ' + data.daily[2].uvi;
+                    
                     // forecast day 4
-                    dayFourTemp.innerHTML = 'Temp: ' + data.daily[3].temp.day;
-                    dayFourWind.innerHTML = 'Wind Speed: ' + data.daily[3].wind_speed;
+                    dayFourDate.innerHTML = dateFour;
+                    dayFourTemp.innerHTML = 'Temp: ' + data.daily[3].temp.day + '°F';
+                    dayFourWind.innerHTML = 'Wind Speed: ' + data.daily[3].wind_speed + ' MPH';
                     dayFourUV.innerHTML = 'UV Index: ' + data.daily[3].uvi;
                     
                 });
