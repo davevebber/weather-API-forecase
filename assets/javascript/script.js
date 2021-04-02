c0e4ca864df43a30cde05c576f48556379617451
dave vebber <davevebber@gmail.com>
2021-04-01 19:34:53 -0700

    refactored code with for loops to be cleaner and easier to read. local storage now saves your previous searches.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,21 +4,18 @@ const searchBtn = document.querySelector('#searchBtn');
 const currentBox = document.querySelector('.current-box');
 const forecastBox = document.querySelector('.forecast-box');
 const searchHistory = document.querySelector('#search-history');
+const currentForecast = document.querySelector('#current-forecast');
+const fourDayForecast = document.querySelector('#four-day-forecast');
+
 
 // dates
 let date = (moment().format('ddd, MMM Do'));
-let dateOne = moment().add(1, 'days').format('MMM Do');
-let dateTwo = moment().add(2, 'days').format('MMM Do');
-let dateThree = moment().add(3, 'days').format('MMM Do');
-let dateFour = moment().add(4, 'days').format('MMM Do');
 
 // variables
-let searchHistoryArr = []
+let searchHistoryArr = JSON.parse(localStorage.getItem('cities')) || []
+
+// icons
 let currentDayIcon = document.querySelector('#currentIcon');
-let dayOneIcon = document.querySelector('#dayOneIcon');
-let dayTwoIcon = document.querySelector('#dayTwoIcon');
-let dayThreeIcon = document.querySelector('#dayThreeIcon');
-let dayFourIcon = document.querySelector('#dayFourIcon');
 
 // for current day weather
 let cityName = document.querySelector('#cityName');
@@ -28,60 +25,15 @@ let humid = document.querySelector('#humid');
 let wind = document.querySelector('#wind');
 let uv = document.querySelector('#uv');
 
+searchBtn.addEventListener('click', getCityWeather);
+
 // UVI warnings
 let yellowWarning = document.querySelector('#uv-warning-yellow');
 let redWarning = document.querySelector('#uv-warning-red');
 
-// for forecast weather
-let dayOneDate = document.querySelector('#dayOneDate');
-let dayOneTemp = document.querySelector('#dayOneTemp');
-let dayOneWind = document.querySelector('#dayOneWind');
-let dayOneUV = document.querySelector('#dayOneUV');
-
-let dayTwoDate = document.querySelector('#dayTwoDate');
-let dayTwoTemp = document.querySelector('#dayTwoTemp');
-let dayTwoWind = document.querySelector('#dayTwoWind');
-let dayTwoUV = document.querySelector('#dayTwoUV');
-
-let dayThreeDate = document.querySelector('#dayThreeDate');
-let dayThreeTemp = document.querySelector('#dayThreeTemp');
-let dayThreeWind = document.querySelector('#dayThreeWind');
-let dayThreeUV = document.querySelector('#dayThreeUV');
-
-let dayFourDate = document.querySelector('#dayFourDate');
-let dayFourTemp = document.querySelector('#dayFourTemp');
-let dayFourWind = document.querySelector('#dayFourWind');
-let dayFourUV = document.querySelector('#dayFourUV');
-
-// for search history
-const firstSearch = document.querySelector('#first-search')
-const secondSearch = document.querySelector('#second-search')
-const thirdSearch = document.querySelector('#third-search')
-const fourthSearch = document.querySelector('#fourth-search')
-const fifthSearch = document.querySelector('#fifth-search')
-
 function displaySearchHistory() {
-    firstSearch.innerHTML = localStorage.getItem(searchHistoryArr[0]);
-
+    firstSearch.innerHTML = localStorage.getItem(searchHistoryArr[1]);
 }
-
-// const info = document.querySelector('#info');
-//             const button  = document.querySelector('#button');
-//             const cityDiv = document.getElementById('cityDiv');
-//             const store = window.localStorage;
-//             let storageArr = [];
-// //             const oldCities = JSON.parse(window.localStorage.getItem("cities")) || [];
-//             button.addEventListener('click', ()=>{
-//                 storageArr.push(info.value);
-//                 store.setItem('cities',JSON.stringify(storageArr));
-//             });
-// for(let i = 0; i < oldCities.length;i++){
-//     cityDiv.innerHTML += <div>${oldCities[i]}</div>;
-// }
-// oldCities.forEach(element => {
-//     cityDiv.innerHTML += <div>${element}</div>;
-// });
-
 
 function getCityWeather() {
     // save searches into array
@@ -89,99 +41,60 @@ function getCityWeather() {
     searchHistoryArr.push(searchCity)
     localStorage.setItem('cities', JSON.stringify(searchHistoryArr));
 
-    // pull cities from array and post on search history
-    firstSearch.innerHTML = searchHistoryArr[0];
-    secondSearch.innerHTML = searchHistoryArr[1];
-    thirdSearch.innerHTML = searchHistoryArr[2];
-    fourthSearch.innerHTML = searchHistoryArr[3];
-    fifthSearch.innerHTML = searchHistoryArr[4];
+    // fetch weather info for current forecast and long+lat for 4 day forecast
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&APPID=' + apiKey + '&units=imperial')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
+            // post current forecast
+            currentForecast.innerHTML = ''
 
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&APPID=' + apiKey + '&units=imperial')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
+            currentForecast.innerHTML =
+                `<h4>${data.name}, ${date}</h4>   
+                       <span><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"></span>
+                        <p>Temperature: ${data.main.temp}°F</p>
+                        <p>Humidity: ${data.main.humidity}%</p>
+                        <p>Wind Speed: ${data.wind.speed} MPH</p>`
 
-                // create icon
-                iconCode = data.weather[0].icon
-                currentDayIcon.src = 'https://openweathermap.org/img/wn/' + iconCode + '.png';
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&units=imperial' + '&APPID=' + apiKey)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
 
-                // current day weather info
-                cityName.innerHTML = data.name + ': ' + date;
+                    currentForecast.innerHTML += `<p>UV Index: ${data.daily[0].uvi}</p>`
+                    if (data.daily[0].uvi == 1000) {
+                        currentForecast.innerHTML +=
+                            `<span id="uv-warning-yellow"><img class="warning-icon"
+                            src="./assets/images/warning-yello.jpg"></span>`
+                    } else {
+                        `<span id="uv-warning-red"><img class="warning-icon"
+                        src="./assets/images/warning-red.png"></span>`
+                    }
 
-                temp.innerHTML = 'Temperature: ' + data.main.temp + '°F';
-                humid.innerHTML = 'Humidity: ' + data.main.humidity + '%';
-                wind.innerHTML = 'Wind Speed: ' + data.wind.speed + ' MPH';
+                    // posting the 4-day-forecast
+                    fourDayForecast.innerHTML = ''
 
-                fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&units=imperial' + '&APPID=' + apiKey)
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        console.log(data);
-                        // current day UV index
-                        uv.innerHTML = 'UV Index: ' + data.current.uvi;
+                    for (let i = 0; i < 4; i++) {
+                        let momentDate = moment().add(i + 1, 'days').format('MMM Do')
 
-                        // which warning icon to display 
-                        if (data.current.uvi < 7) {
-                            yellowWarning.classList.remove('hide');
-                        } else {
-                            redWarning.classList.remove('hide');
-                        }
+                        fourDayForecast.innerHTML +=
+                            `<div id="card" class="col-3">
+                            <p>${momentDate}</p>
+                            <span><img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png"></span>
+                            <p>Temp:${data.daily[i].temp.day}°F</p>
+                            <p>Wind Speed:${data.daily[i].wind_speed}MPH</p>
+                            <p>UV Index:${data.daily[i].uvi}</p>
+                            </div>`
+                    };
 
-                        // icons for forecast day 1
-                        dayOneIconCode = data.daily[0].weather[0].icon
-                        dayOneIcon.src = 'https://openweathermap.org/img/wn/' + dayOneIconCode + '.png';
+                    currentBox.classList.remove('hide');
+                    forecastBox.classList.remove('hide');
 
-                        // icons for forecast day 2
-                        dayTwoIconCode = data.daily[1].weather[0].icon
-                        dayTwoIcon.src = 'https://openweathermap.org/img/wn/' + dayTwoIconCode + '.png';
-
-                        // icons for forecast day 3
-                        dayThreeIconCode = data.daily[2].weather[0].icon
-                        dayThreeIcon.src = 'https://openweathermap.org/img/wn/' + dayThreeIconCode + '.png';
-
-                        // icons for forecast day 4
-                        dayFourIconCode = data.daily[3].weather[0].icon
-                        dayFourIcon.src = 'https://openweathermap.org/img/wn/' + dayFourIconCode + '.png';
-
-                        // forecast day 1
-                        dayOneDate.innerHTML = dateOne;
-
-                        dayOneIconCode = data.daily[0].weather[0].icon
-                        dayOneIcon.src = 'https://openweathermap.org/img/wn/' + dayOneIconCode + '.png';
-
-                        dayOneTemp.innerHTML = 'Temp: ' + data.daily[0].temp.day + '°F';
-                        dayOneWind.innerHTML = 'Wind Speed: ' + data.daily[0].wind_speed + ' MPH';
-                        dayOneUV.innerHTML = 'UV Index: ' + data.daily[0].uvi;
-
-                        // forecast day 2
-                        dayTwoDate.innerHTML = dateTwo;
-                        dayTwoTemp.innerHTML = 'Temp: ' + data.daily[1].temp.day + '°F';
-                        dayTwoWind.innerHTML = 'Wind Speed: ' + data.daily[1].wind_speed + ' MPH';
-                        dayTwoUV.innerHTML = 'UV Index: ' + data.daily[1].uvi;
-
-                        // forecast day 3
-                        dayThreeDate.innerHTML = dateThree;
-                        dayThreeTemp.innerHTML = 'Temp: ' + data.daily[2].temp.day + '°F';
-                        dayThreeWind.innerHTML = 'Wind Speed: ' + data.daily[2].wind_speed + ' MPH';
-                        dayThreeUV.innerHTML = 'UV Index: ' + data.daily[2].uvi;
-
-                        // forecast day 4
-                        dayFourDate.innerHTML = dateFour;
-                        dayFourTemp.innerHTML = 'Temp: ' + data.daily[3].temp.day + '°F';
-                        dayFourWind.innerHTML = 'Wind Speed: ' + data.daily[3].wind_speed + ' MPH';
-                        dayFourUV.innerHTML = 'UV Index: ' + data.daily[3].uvi;
-                    });
-            });
-        currentBox.classList.remove('hide');
-        forecastBox.classList.remove('hide');
-    };
-
-
-
-
-
-    searchBtn.addEventListener('click', getCityWeather);
+                });
+        })
+};
